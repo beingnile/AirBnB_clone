ab8333e9416eef299d811329aafc34c94cab0789
Nile Okomo <okomonile3@gmail.com>
2022-01-27 02:12:50 +0300

    Update Docstrings in module

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
+
+"""Defines a base class BaseModel"""
 import uuid
 import datetime
 
 
 class BaseModel:
+    """Defines all common attributes for other classes"""
     def __init__(self):
+        """Runs on instantiation of BaseModel class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
+        """Overwrites the inbuilt __str__ method
+
+        Returns:
+            [<class name>] (<self.id>) <self.__dict__>
+        """
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """Updates attribute updated_at with the current datetime"""
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
+        """
+        Adds a key __class__ with the class name of object
+        Converts created_at and updated_at to string object in ISO format
+
+        Returns:
+            A dictionary containing all keys/values of __dict__ of the instance
+        """
         self.__dict__['created_at'] = self.created_at.isoformat()
         self.__dict__['updated_at'] = self.updated_at.isoformat()
         self.__dict__['__class__'] = type(self).__name__

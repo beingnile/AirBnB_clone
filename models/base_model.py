2611f39d9f238f5fa20297078898889045fb1a65
Nile Okomo <okomonile3@gmail.com>
2022-01-26 23:32:17 +0300

    Write Base Class to be inherited from

---
+++ models/base_model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        

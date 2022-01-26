dd1e618783dc85e0e1526622aab9954e35f4e92d
Nile Okomo <okomonile3@gmail.com>
2022-01-27 00:51:41 +0300

    Overwrite __str__ method

--- models/base_model.py
+++ models/base_model.py
@@ -6,4 +6,7 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        
+        self.updated_at = datetime.datetime
+
+    def __str__(self):
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"

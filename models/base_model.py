c66c02ea23b04f48ffb4b13449cac1ebb3b9e8e5
Nile Okomo <okomonile3@gmail.com>
2022-01-27 01:05:27 +0300

    Fix updated_at attribute | Add new method to class

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,13 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        pass

b79e078a4dc3be072db066f7244b3f447ef1b074
Nile Okomo <okomonile3@gmail.com>
2022-01-27 20:36:07 +0300

    Update __init__ to take kwargs arguments

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ import datetime
 
 class BaseModel:
     """Defines all common attributes for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Runs on instantiation of BaseModel class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()

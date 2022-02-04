0128d8d93ca35ea454a20b22c276a4848df134c1
Nile Okomo <okomonile3@gmail.com>
2022-02-04 13:29:07 +0300

    Define sformat variable

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ class BaseModel:
     """Defines all common attributes for other classes"""
     def __init__(self, *args, **kwargs):
         """Runs on instantiation of BaseModel class"""
+        sformat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()

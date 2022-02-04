209b743a7c1d6e85db269f0ffe9a85b42c2ef52d
Nile Okomo <okomonile3@gmail.com>
2022-02-04 13:03:40 +0300

    Fix linter errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 import json
 
+
 class FileStorage:
     __file_path = ""
     __objects = {}
 
     def all(self):
         return FileStorage.__objects
+
     def new(self, obj):
         FileStorage.__objects['obj'] = f'type(self).__name__'
+
     def save(self):
         json.dump(FileStorage.__objects, FileStorage.__file_path)

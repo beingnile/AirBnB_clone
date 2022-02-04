1d911236e0c831c3b817333ba2b4a514b9221c2d
Nile Okomo <okomonile3@gmail.com>
2022-02-04 13:27:09 +0300

    Handle kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -12,6 +12,16 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.datetime.strptime(value, sformat)
+                else:
+                    self.__dict__[key] = value:
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+
 
     def __str__(self):
         """Overwrites the inbuilt __str__ method
@@ -33,7 +43,7 @@ class BaseModel:
         Returns:
             A dictionary containing all keys/values of __dict__ of the instance
         """
-        my_dict = self.__dict__
+        my_dict = self.__dict__.copy()
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
         my_dict['__class__'] = type(self).__name__

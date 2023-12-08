1d0684b8a9979d3caa8900257db3dce3ccf19d5e
Nile Okomo <okomonile3@gmail.com>
2023-12-08 17:47:48 +0300

    Update BaseModel to load objects from kwargs if not empty

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,19 @@ import uuid
 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Loads an object from kwargs if not empty"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Overwrites __str__ to print

6116888c1daf77233eb5938f73822d38be20a04d
Nile Okomo <okomonile3@gmail.com>
2023-12-11 00:40:19 +0300

    Implement create, show and destroy commands to handle models

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Defines a cmd prompt"""
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,6 +22,64 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Override empty line to do nothing"""
         pass
+
+    def do_create(self, arg):
+        """Creates a new instance of a model
+        and saves it to a JSON file
+        """
+        if not arg:
+            print("** class name missing **")
+        elif arg == 'BaseModel':
+            b = BaseModel()
+            b.save()
+            print(b.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an
+        instance based on the class name and id
+        """
+        if not arg:
+            print("** class name missing **")
+        args = arg.split()
+        if len(args) == 1:
+            print("** instance id missing **")
+        elif len(args) == 2:
+            if args[0] == 'BaseModel':
+                key = args[0] + '.' + args[1]
+                storage.reload()
+                my_objs = storage.all()
+                try:
+                    my_dict_obj = my_objs[key]
+                    model = BaseModel(**my_dict_obj)
+                    print(model)
+                except KeyError:
+                    print("** no instance found **")
+            else:
+                print("** class doesn't exist **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the
+        class name and id
+        """
+        if not arg:
+            print("** class name missing **")
+        args = arg.split()
+        if len(args) == 1:
+            print("** instance id missing **")
+        elif len(args) == 2:
+            if args[0] == 'BaseModel':
+                key = args[0] + '.' + args[1]
+                storage.reload()
+                try:
+                    del(storage.all()[key])
+                    storage.save()
+                except KeyError:
+                    print("** no instance found **")
+            else:
+                print("** class doesn't exist **")
 
 
 if __name__ == '__main__':

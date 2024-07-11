65227e820e63b2c5f96461d031a2efb4a5ed2243
Nile Okomo <okomonile3@gmail.com>
2024-07-11 22:14:19 +0300

    Update unittests for state model | Add test for saving in json file

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,52 +1,69 @@
 #!/usr/bin/python3
 """Defines testcases for state model"""
+import json
+import os
 import unittest
-from models.state import State
+from models import storage
 from models.base_model import BaseModel
+from models.state import State
 
 
 class TestState(unittest.TestCase):
     """Test cases for the State class"""
     def setUp(self):
-        """Set up test methods"""
+        """Sets up test methods"""
         self.state = State()
 
+    def tearDown(self):
+        """Tears down test methods"""
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+
     def test_instance(self):
-        """Test that state is an instance of BaseModel"""
+        """Tests that state is an instance of BaseModel"""
         self.assertIsInstance(self.state, BaseModel)
 
     def test_default_attributes(self):
-        """Test default attributes are empty strings"""
+        """Tests default attributes are empty strings"""
         self.assertEqual(self.state.name, "")
 
     def test_set_attributes(self):
-        """Test setting attributes"""
+        """Tests setting attributes"""
         self.state.name = "California"
         self.assertEqual(self.state.name, "California")
 
     def test_attributes_types(self):
-        """Test attribute types"""
+        """Tests attribute types"""
         self.assertIsInstance(self.state.name, str)
 
     def test_none_attributes(self):
-        """Test setting attributes to None"""
+        """Tests setting attributes to None"""
         self.state.name = None
         self.assertIsNone(self.state.name)
 
     def test_save_method(self):
-        """Test save method from BaseModel"""
+        """Tests save method from BaseModel"""
         old_updated_at = self.state.updated_at
         self.state.save()
         self.assertNotEqual(old_updated_at, self.state.updated_at)
 
     def test_to_dict_method(self):
-        """Test to_dict method from BaseModel"""
+        """Tests to_dict method from BaseModel"""
         self.state.name = "California"
         state_dict = self.state.to_dict()
         self.assertEqual(state_dict['name'], "California")
         self.assertEqual(state_dict['__class__'], 'State')
         self.assertIsInstance(state_dict['created_at'], str)
         self.assertIsInstance(state_dict['updated_at'], str)
+
+    def test_save_to_file(self):
+        """Test if the state is saved to the file"""
+        self.state.save()
+        self.assertTrue(os.path.exists("file.json"))
+        with open("file.json", "r") as file:
+            data = json.load(file)
+            key = f"State.{self.state.id}"
+            self.assertIn(key, data)
 
 
 if __name__ == '__main__':

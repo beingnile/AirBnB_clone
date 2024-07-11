fcacb36bccc0d05d20040f7efe3b61b1f6ed6c73
Nile Okomo <okomonile3@gmail.com>
2024-07-11 22:16:43 +0300

    Update unittests for amenity model | Add test for saving in json file

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,52 +1,68 @@
 #!/usr/bin/python3
 """Defines testcases for amenity model"""
+import json
+import os
 import unittest
 from models.amenity import Amenity
 from models.base_model import BaseModel
 
 
 class TestAmenity(unittest.TestCase):
-    """Test cases for the Amenity class"""
+    """Tests cases for the Amenity class"""
     def setUp(self):
-        """Set up test methods"""
+        """Sets up test methods"""
         self.amenity = Amenity()
 
+    def tearDown(self):
+        """Tears down test methods"""
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+
     def test_instance(self):
-        """Test that amenity is an instance of BaseModel"""
+        """Tests that amenity is an instance of BaseModel"""
         self.assertIsInstance(self.amenity, BaseModel)
 
     def test_default_attributes(self):
-        """Test default attributes are empty strings"""
+        """Tests default attributes are empty strings"""
         self.assertEqual(self.amenity.name, "")
 
     def test_set_attributes(self):
-        """Test setting attributes"""
+        """Tests setting attributes"""
         self.amenity.name = "Wifi"
         self.assertEqual(self.amenity.name, "Wifi")
 
     def test_attributes_types(self):
-        """Test attribute types"""
+        """Tests attribute types"""
         self.assertIsInstance(self.amenity.name, str)
 
     def test_none_attributes(self):
-        """Test setting attributes to None"""
+        """Tests setting attributes to None"""
         self.amenity.name = None
         self.assertIsNone(self.amenity.name)
 
     def test_save_method(self):
-        """Test save method from BaseModel"""
+        """Tests save method from BaseModel"""
         old_updated_at = self.amenity.updated_at
         self.amenity.save()
         self.assertNotEqual(old_updated_at, self.amenity.updated_at)
 
     def test_to_dict_method(self):
-        """Test to_dict method from BaseModel"""
+        """Tests to_dict method from BaseModel"""
         self.amenity.name = "Wifi"
         amenity_dict = self.amenity.to_dict()
         self.assertEqual(amenity_dict['name'], "Wifi")
         self.assertEqual(amenity_dict['__class__'], 'Amenity')
         self.assertIsInstance(amenity_dict['created_at'], str)
         self.assertIsInstance(amenity_dict['updated_at'], str)
+
+    def test_save_to_file(self):
+        """Test if the amenity is saved to the file"""
+        self.amenity.save()
+        self.assertTrue(os.path.exists("file.json"))
+        with open("file.json", "r") as file:
+            data = json.load(file)
+            key = f"Amenity.{self.amenity.id}"
+            self.assertIn(key, data)
 
 
 if __name__ == '__main__':
